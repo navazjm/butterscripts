18206582e22df445439f65676883e903dcafe189
drewgrif <jag@justaguylinux.com>
2025-03-28 19:17:51 -0400

    Update 00-bash.sh

--- setup/00-bash.sh
+++ setup/00-bash.sh
@@ -3,4 +3,4 @@ set -e
 # 00-base.sh: System update and base packages
 
 sudo apt clean && sudo apt update && sudo apt upgrade -y
-sudo apt install -y git curl build-essential xorg xorg-dev xbacklight xbindkeys xvkbd xinput xdg-user-dirs-gtk cmake meson ninja-build pkg-config
+sudo apt install -y git curl build-essential xorg xorg-dev xbacklight xbindkeys xvkbd xinput xdg-user-dirs-gtk cmake meson ninja-build pkg-config network-manager-gnome pamixer dialog mtools avahi-daemon acpi acpid gvfs-backends cifs-utils pipewire-audio unzip
