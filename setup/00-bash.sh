92fdf91883237220b96d8c69d4fc2180266fae23
drewgrif <jag@justaguylinux.com>
2025-03-28 19:10:01 -0400

    Update 00-bash.sh

--- setup/00-bash.sh
+++ setup/00-bash.sh
@@ -2,6 +2,5 @@
 set -e
 # 00-base.sh: System update and base packages
 
-sudo apt update
-sudo apt upgrade -y
+sudo apt clean && sudo apt update && sudo apt upgrade -y
 sudo apt install -y git curl build-essential xorg xorg-dev xbacklight xbindkeys xvkbd xinput xdg-user-dirs-gtk cmake meson ninja-build pkg-config
