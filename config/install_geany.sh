2b1df9e30920452f55700040204d26c770973900
JustAGuyLinux <jag@justaguylinux.com>
2025-05-18 14:36:12 -0400

    renaming

---
+++ config/install_geany.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+geany geany-plugin-addons geany-plugin-git-changebar geany-plugin-spellcheck geany-plugin-treebrowser geany-plugin-markdown geany-plugin-insertnum geany-plugin-lineoperations geany-plugin-automark 
