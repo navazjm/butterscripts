576f60d37721cbfe8fffc5254d97975b77924abd
JustAGuyLinux <jag@justaguylinux.com>
2025-05-22 15:07:04 -0400

    Update add_bashrc.sh

--- system/add_bashrc.sh
+++ system/add_bashrc.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-echo "Would you like to overwrite your current .bashrc with the justaguylinux .bashrc? (y/n)"
-read response
-
+read -p "Replace your .bashrc with justaguylinux .bashrc? (y/n) " response
 if [[ "$response" =~ ^[Yy]$ ]]; then
-    if [[ -f ~/.bashrc ]]; then
-        mv ~/.bashrc ~/.bashrc.bak
-        echo "Your current .bashrc has been moved to .bashrc.bak"
-    fi
-    wget -O ~/.bashrc https://raw.githubusercontent.com/drewgrif/jag_dots/main/.bashrc
-    source ~/.bashrc
-    if [[ $? -eq 0 ]]; then
-        echo "justaguylinux .bashrc has been copied to ~/.bashrc"
-    else
-        echo "Failed to download justaguylinux .bashrc"
-    fi
-elif [[ "$response" =~ ^[Nn]$ ]]; then
-    echo "No changes have been made to ~/.bashrc"
-else
-    echo "Invalid input. Please enter 'y' or 'n'."
+    [[ -f ~/.bashrc ]] && mv ~/.bashrc ~/.bashrc.bak
+    wget -O ~/.bashrc https://raw.githubusercontent.com/drewgrif/jag_dots/main/.bashrc && source ~/.bashrc
 fi
