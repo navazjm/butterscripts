052924199ec3c785b6d1d3c562c1f32ddbaeb22a
JustAGuyLinux <jag@justaguylinux.com>
2025-06-15 12:12:21 -0400

    Update add_bashrc.sh

--- system/add_bashrc.sh
+++ system/add_bashrc.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-if [ -t 0 ]; then
-    read -p "Replace your .bashrc with justaguylinux .bashrc? (y/n) " -n 1 -r
-    echo
-    [[ $REPLY =~ ^[Yy]$ ]] || exit 0
-fi
-
 [ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.bak
 wget -O ~/.bashrc https://raw.githubusercontent.com/drewgrif/jag_dots/main/.bashrc
